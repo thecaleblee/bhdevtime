c0f05d7540c2052773831a9b966a493b5c1dd9ea
Caleb Masters <caleb.masters@endurance.com>
2019-02-22 06:59:21 -0800

    Moved a new folder into repo to try and get past build issue

---
+++ bhReact/src/pages/wordpress-hosting.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import Layout from '../components/layout'
+import { graphql } from "gatsby"
+import SEO from '../components/seo'
+import '../pages/wordpress-hosting.css'
+import Hero from '../components/Hero'
+
+const WordpressHostingPage = ({ data, children }) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <div className="site-wrapper">
+      <div className="site-canvas">
+        <Hero data={data}>
+          vat disclaimer
+        </Hero>
+      </div>
+    </div>
+  </Layout>
+)
+
+export const query = graphql`
+  query wpHostingQuery {
+    contentfulTestHero {
+      title
+      subtitle
+      featuresList
+      marketingSlang
+      linkText
+      linkHref
+      startingPriceUsd
+      monthlyPriceUsd
+      heroBackgroundMobile {
+        file {
+          url
+        }
+      }
+      heroBackgroundTablet {
+        file {
+          url
+        }
+      }
+      heroBackgroundDesktop {
+        file {
+          url
+        }
+      }
+    }
+    contentfulContentfulTestIconGroup {
+      iconTitles
+      iconText
+      iconImages {
+        file {
+          url
+        }
+        title
+      }
+    }
+    contentfulFeatureGrid {
+      featureGridTitle
+      featureIcon1 {
+        file {
+          url
+        }
+      }
+      featureHeading1
+      featureText1
+      featureIcon2 {
+        file {
+          url
+        }
+      }
+      featureHeading2
+      featureText2
+      featureIcon3 {
+        file {
+          url
+        }
+      }
+      featureHeading3
+      featureText3
+      featureIcon4 {
+        file {
+          url
+        }
+      }
+      featureHeading4
+      featureText4
+      featureIcon5 {
+        file {
+          url
+        }
+      }
+      featureHeading5
+      featureText5
+      featureIcon6 {
+        file {
+          url
+        }
+      }
+      featureHeading6
+      featureText6
+    }
+  }
+`
+
+export default WordpressHostingPage
